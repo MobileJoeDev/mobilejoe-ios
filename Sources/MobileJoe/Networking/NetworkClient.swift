4418cd912174c5c764cd86a8b16820f37d616f10
Florian Mielke <github@intialwert.de>
2025-03-20 17:36:46 +0100

    Refactoring

--- Sources/MobileJoe/Networking/NetworkClient.swift
+++ Sources/MobileJoe/Networking/NetworkClient.swift
@@ -10,45 +10,53 @@ import Foundation
 @MainActor
 class NetworkClient {
   static let shared = NetworkClient()
-
+  
   @discardableResult
   static func configure(withAPIKey apiKey: String, appUserID: String) -> NetworkClient {
     shared.apiKey = apiKey
     return shared
   }
-
+  
   private var apiKey: String = ""
   private var appUserID: String = ""
-
+  
   private static let serverHostURL = URL(string: "https://api.mobilejoe.dev")!
   private static let apiVersion = "v1"
-
+  
   func getFeatureRequests() async throws -> (Data, URLResponse) {
-    var urlComponents = URLComponents()
-    urlComponents.scheme = Self.serverHostURL.scheme
-    urlComponents.host = Self.serverHostURL.host
-    urlComponents.path = "/\(Self.apiVersion)/feature_requests"
-    urlComponents.queryItems = [
-      URLQueryItem(name: "user_id", value: appUserID.escaped)
-    ]
-
-    var request = URLRequest(url: urlComponents.url!)
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+    var components = try url(for: "feature_requests")
+    components.queryItems = [URLQueryItem(name: "user_id", value: appUserID.escaped)]
+    guard let url = components.url else {
+      throw MobileJoeError.generic("Invalid URL")
+    }
+    var request = urlRequest(for: url)
     request.httpMethod = "GET"
     return try await URLSession.shared.data(for: request)
   }
-
+  
   func postVoteFeatureRequests(featureRequestID: Int) async throws -> (Data, URLResponse) {
-    var urlComponents = URLComponents()
-    urlComponents.scheme = Self.serverHostURL.scheme
-    urlComponents.host = Self.serverHostURL.host
-    urlComponents.path = "/\(Self.apiVersion)/feature_requests/\(featureRequestID)/vote"
-    var request = URLRequest(url: urlComponents.url!)
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+    let components = try url(for: "feature_requests/\(featureRequestID)/vote")
+    guard let url = components.url else {
+      throw MobileJoeError.generic("Invalid URL")
+    }
+    var request = urlRequest(for: url)
     request.httpBody = try JSONEncoder().encode(["user_id": appUserID.escaped])
     request.httpMethod = "POST"
     return try await URLSession.shared.data(for: request)
+  }
+  
+  private func urlRequest(for url: URL) -> URLRequest {
+    var request = URLRequest(url: url)
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+    return request
+  }
+  
+  private func url(for path: String) throws -> URLComponents {
+    var urlComponents = URLComponents()
+    urlComponents.scheme = Self.serverHostURL.scheme
+    urlComponents.host = Self.serverHostURL.host
+    urlComponents.path = "/\(Self.apiVersion)/\(path)"
+    return urlComponents
   }
 }
