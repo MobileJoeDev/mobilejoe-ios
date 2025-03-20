e4d39a0342b78f21fdb39b854c80b813eb1afcee
Florian Mielke <github@intialwert.de>
2025-03-20 17:28:23 +0100

    Move API calls to NetworkClient

--- Sources/MobileJoe/MobileJoe.swift
+++ Sources/MobileJoe/MobileJoe.swift
@@ -22,19 +22,12 @@ public class MobileJoe {
 
   @discardableResult
   public static func configure(withAPIKey apiKey: String, appUserID: String) -> MobileJoe {
-    shared.apiKey = apiKey
-    shared.appUserID = appUserID.escaped
+    NetworkClient.configure(withAPIKey: apiKey, appUserID: appUserID)
     return shared
   }
 
   public var featureRequests: [FeatureRequest] = []
 
-  private var apiKey: String = "" {
-    didSet {
-      NetworkClient.configure(withAPIKey: apiKey)
-    }
-  }
-  private var appUserID: String = ""
   private var featureRequestContainer = Set<FeatureRequest>()
   private let logger = Logger(subsystem: "MobileJoe", category: "MobileJoe")
 
@@ -42,22 +35,12 @@ public class MobileJoe {
   }
 
   public func fetchFeatureRequests() async throws {
-    let url = URL(string: "https://api.mobilejoe.dev/v1/feature_requests")!
-    var request = URLRequest(url: url)
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
-    let result = try await URLSession.shared.data(for: request)
-    try await handle(result)
+    let response = try await NetworkClient.shared.getFeatureRequests()
+    try await handle(response)
   }
 
   public func vote(for featureRequest: FeatureRequest) async throws {
-    let url = URL(string: "https://api.mobilejoe.dev/v1/feature_requests/\(featureRequest.id)/vote")!
-    var request = URLRequest(url: url)
-    request.httpMethod = "POST"
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
-    request.httpBody = try JSONEncoder().encode(["user_id": appUserID])
-    let response = try await URLSession.shared.data(for: request)
+    let response = try await NetworkClient.shared.postVoteFeatureRequests(featureRequestID: featureRequest.id)
     guard is200Response(response.1) else {
       throw MobileJoeError.generic("Invalid response: \(response.1)")
     }
--- Sources/MobileJoe/Networking/NetworkClient.swift
+++ Sources/MobileJoe/Networking/NetworkClient.swift
@@ -12,28 +12,43 @@ class NetworkClient {
   static let shared = NetworkClient()
 
   @discardableResult
-  static func configure(withAPIKey apiKey: String) -> NetworkClient {
+  static func configure(withAPIKey apiKey: String, appUserID: String) -> NetworkClient {
     shared.apiKey = apiKey
     return shared
   }
 
+  private var apiKey: String = ""
+  private var appUserID: String = ""
+
   private static let serverHostURL = URL(string: "https://api.mobilejoe.dev")!
   private static let apiVersion = "v1"
 
-  private var apiKey: String = ""
-  private var appUserID: String = ""
-}
+  func getFeatureRequests() async throws -> (Data, URLResponse) {
+    var urlComponents = URLComponents()
+    urlComponents.scheme = Self.serverHostURL.scheme
+    urlComponents.host = Self.serverHostURL.host
+    urlComponents.path = "/\(Self.apiVersion)/feature_requests"
+    urlComponents.queryItems = [
+      URLQueryItem(name: "user_id", value: appUserID.escaped)
+    ]
 
-extension NetworkClient {
-  enum Request {
-    case getFeatureRequests(appUserID: String)
-    case postVoteFeatureRequests(featureRequestID: String)
+    var request = URLRequest(url: urlComponents.url!)
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+    request.httpMethod = "GET"
+    return try await URLSession.shared.data(for: request)
+  }
 
-    var pathComponent: String {
-      switch self {
-      case .getFeatureRequests(let appUserID): "/feature_requests?user_id?=\(appUserID.escaped)"
-      case .postVoteFeatureRequests(let featureRequestID): "feature_requests/\(featureRequestID.escaped)/vote"
-      }
-    }
+  func postVoteFeatureRequests(featureRequestID: Int) async throws -> (Data, URLResponse) {
+    var urlComponents = URLComponents()
+    urlComponents.scheme = Self.serverHostURL.scheme
+    urlComponents.host = Self.serverHostURL.host
+    urlComponents.path = "/\(Self.apiVersion)/feature_requests/\(featureRequestID)/vote"
+    var request = URLRequest(url: urlComponents.url!)
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+    request.httpBody = try JSONEncoder().encode(["user_id": appUserID.escaped])
+    request.httpMethod = "POST"
+    return try await URLSession.shared.data(for: request)
   }
 }
